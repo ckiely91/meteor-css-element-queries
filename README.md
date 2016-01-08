a7d2e88e0e23f00422925a6243eb9085dd468c9f
Christian Kiely <ckiely91@gmail.com>
2016-01-08 14:26:56 +1100

    Update username

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Simple Meteor wrapper for markj's [CSS Element Queries](https://github.com/marcj/css-element-queries).
 
-`meteor add ckiely:css-element-queries`
+`meteor add ckiely91:css-element-queries`
--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-	name: "ckiely:css-element-queries"
+	name: "ckiely91:css-element-queries"
 	summary: "Simple wrapper for CSS Element Queries",
 	version: "0.0.1",
 	git: "https://github.com/ckiely91/meteor-css-element-queries"
